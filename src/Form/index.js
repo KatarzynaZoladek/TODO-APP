4cbb25895ad0d826b8941fc93f0b17e84b277f8b
KatarzynaZoladek <katarzynawojcik47@gmail.com>
2024-02-13 18:15:09 +0100

    task list without interactions yet

---
+++ src/Form/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const Form = () => (
+    <form className="form">
+        <input
+            type="text"
+            id="input"
+            name=""
+            className="form__input"
+            placeholder="Co jest do zrobienia?"
+            autofocus
+        />
+        <button className="form__button">Dodaj zadanie</button>
+    </form>
+);
+
+export default Form;
