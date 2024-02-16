fb9b3c85068df7d93208e5063634759b246084e6
KatarzynaZoladek <katarzynawojcik47@gmail.com>
2024-02-16 19:16:27 +0100

    toggleHideDone interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,24 +1,22 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone }) => {
-  if (tasks.length === 0) {
-    return null
-  }
-
-  return (
-    <div className="buttons">
-      <button className="buttons__button">
-        {hideDone ? "Pokaż" : "Ukryj"} ukończone
-      </button>
-      <button
-        className="buttons__button"
-        disabled={tasks.every(({ done }) => done)}
-      >
-        Ukończ wszystkie
-      </button>
-    </div>
-  )
-};
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+  <div className="buttons">
+    {tasks.length > 0 && (
+      <>
+        <button onClick={toggleHideDone} className="buttons__button">
+          {hideDone ? "Pokaż" : "Ukryj"} ukończone
+        </button>
+        <button
+          className="buttons__button"
+          disabled={tasks.every(({ done }) => done)}
+        >
+          Ukończ wszystkie
+        </button>
+      </>
+    )}
+  </div>
+);
 
 export default Buttons;
